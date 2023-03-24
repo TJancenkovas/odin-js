3e52cd72dc007d4917935322a07338c2b9acb258
TJancenkovas <t.jancenkovas@gmail.com>
2023-03-24 14:52:27 +0200

    Adapted variables to SOE

--- learning/variables.js
+++ learning/variables.js
@@ -12,6 +12,7 @@ alert(message);
 
 
 //Declaring constants
+    //Use uppercase for hardcoded variables - variables wiht a known name beforehand.
 
 const TOMBDAY = '1998.03.03';
 
@@ -24,3 +25,28 @@ name = "John";
 admin = name;
 
 alert(admin);
+
+//Task 2
+
+let ourPlanetName = "Earth",
+    currentUserName = "Janny";
+
+//Odin tasks
+    //Task 1
+        console.log(23+97);
+    //Task 2
+        console.log(1+2+3+4+5+6);
+    //Task 3 
+        console.log((4+6+9)/77);
+    //Task 4
+        let a = 10;
+        console.log(a);
+        console.log(a*9);
+        let b = 7 * a;
+        console.log(b);
+    //Task 5 
+        let max = 57,
+            actual = max-13,
+            percentage = actual / max;
+        console.log(percentage*100 + "%");
+    //Task 6
