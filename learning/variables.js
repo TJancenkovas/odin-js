b42363b6a81d7429f43b045b2d9a5d2fd2040b29
TJancenkovas <t.jancenkovas@gmail.com>
2023-03-24 11:36:30 +0200

    Add variable lessons

---
+++ learning/variables.js
@@ -0,0 +1,26 @@
+//Declaring any variable
+
+let message;
+message = "Hello";
+
+let user = "Tom",
+    age = 24,
+    alive = 1;
+
+
+alert(message);
+
+
+//Declaring constants
+
+const TOMBDAY = '1998.03.03';
+
+
+//Task
+
+let admin, name;
+
+name = "John";
+admin = name;
+
+alert(admin);
