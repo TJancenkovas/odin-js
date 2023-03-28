98ac3468828ed3229172f1f9b533a989ebca662a
TJancenkovas <t.jancenkovas@gmail.com>
2023-03-28 22:01:30 +0300

    Add RPS pseudocode

---
+++ learning/Projects/RPS.js
@@ -0,0 +1,8 @@
+//Generate RPS choice (0 - rock, 1 - paper, 2 - scissors)
+//Get user RPS choice
+//If generated == user choice - tie
+//Else SWITCH between user choices
+    //Case - user chooses 0
+    //If generated == 1 loose
+    //If generated == 2 win
+    //If generated == 
